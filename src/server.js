9cf8ada49a795c566ec70eba76fc29e9eef8b467
Alexander Kovalke <mgtherion@Alexanders-MacBook-Pro.local>
2018-05-08 01:00:37 +0300

    server updated with suggest page

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@
 import path from 'path';
 import Express from 'express';
 import React from 'react';
-//import mongodb from 'mongodb';
+import isUrl from 'is-url';
+import request from 'request';
+import cheerio from 'cheerio';
+import mongodb from 'mongodb';
 import { Server } from 'http';
 import { renderToString } from 'react-dom/server';
-import { StaticRouter as Router } from 'react-router-dom';
-import App from './components/App';
+import SuggestPage from './components/SuggestPage';
 
 
 const app = new Express();
@@ -17,22 +19,46 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(Express.static(path.join(__dirname, 'static')));
 
-app.get('*', (req, res) => {
-    let status = 200;
+const isArticleValid = (param) => {
+    if (!param) return {'error': 'missing articleUrl parameter'};
+    if (!isUrl(param)) return {'error': 'articleUrl should be url'};
+    if (param.indexOf('dagbladet.no') === -1) return {'error': 'dagbladet functionality only'};
+    return false;
+}
 
-    let markup = renderToString(
-        <Router location={req.url}>
-            <App />
-        </Router>
-    );
+app.get('/fb', (req, res) => {
+    let data = {
+        title: '',
+        pharagraphs: []
+    };
+    let url = req.query.articleUrl;
+    let err = isArticleValid(url);
+    if (err) return res.status(400).json(err);
 
-    return res.status(status).render('index', { markup });
+    request(url, (error, response, body) => {
+        if (error) throw error;
+        var $ = cheerio.load(body);
+
+        data.title = $('h2.headline').text();
+        $("div[itemprop='articleBody'] > p").each(function() {
+            data.pharagraphs.push($(this).text());
+        });
+        let markup = renderToString(
+            <SuggestPage
+                data={data}
+            />
+        );
+
+        return res.status(200).render('index', { markup });
+    });
 });
 
-const port = process.env.PORT || 3003;
+
+
+const port = process.env.PORT || 3000;
 server.listen(port, (err) => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.info(`Server running on http://localhost:${port}`);
+    if (err) {
+        return console.error(err);
+    }
+    return console.info(`Server running on http://localhost:${port}`);
 });
